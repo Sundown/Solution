fcb285f1088f19e5cd2bbcf5f2af9b7509a76ae2
sundown <n@sundow.nl>
2021-12-17 00:24:56 +1300

    move weave structures to prism

---
+++ com/prism/environment.go
@@ -0,0 +1,44 @@
+package prism
+
+import (
+	"github.com/llir/llvm/ir/types"
+)
+
+func NewEnvironment() *Environment {
+	var env Environment
+	env.Functions = make(map[Ident]Function)
+	env.Types = make(map[Ident]Type)
+
+	env.Types[ParseIdent("Int")] = AtomicType{
+		ID:           TypeInt,
+		WidthInBytes: 8,
+		Name:         ParseIdent("Int"),
+		Actual:       types.I64,
+	}
+	env.Types[ParseIdent("Real")] = AtomicType{
+		ID:           TypeReal,
+		WidthInBytes: 8,
+		Name:         ParseIdent("Real"),
+		Actual:       types.Double,
+	}
+	env.Types[ParseIdent("Char")] = AtomicType{
+		ID:           TypeChar,
+		WidthInBytes: 1,
+		Name:         ParseIdent("Char"),
+		Actual:       types.I8,
+	}
+	env.Types[ParseIdent("Bool")] = AtomicType{
+		ID:           TypeBool,
+		WidthInBytes: 1,
+		Name:         ParseIdent("Bool"),
+		Actual:       types.I1,
+	}
+	env.Types[ParseIdent("Void")] = AtomicType{
+		ID:           TypeVoid,
+		WidthInBytes: 0,
+		Name:         ParseIdent("Void"),
+		Actual:       types.Void,
+	}
+
+	return &env
+}
---
+++ com/prism/palisade.go
@@ -0,0 +1,41 @@
+package prism
+
+import (
+	"sundown/solution/palisade"
+
+	"github.com/alecthomas/participle/v2"
+)
+
+func Intern(i palisade.Ident) (p Ident) {
+	if i.Namespace == nil {
+		p.Package = "_"
+	} else {
+		p.Package = *i.Namespace
+	}
+
+	p.Name = *i.Ident
+	return
+}
+
+func ParseIdent(s string) (p Ident) {
+	var t palisade.Ident
+	err := participle.MustBuild(
+		&palisade.Ident{},
+		participle.UseLookahead(4),
+		participle.Unquote()).
+		ParseString("", s, &t)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return Intern(t)
+}
+
+func (env Environment) GetFunction(i Ident) *Function {
+	if f, ok := env.Functions[i]; ok {
+		return &f
+	}
+
+	return nil
+}
