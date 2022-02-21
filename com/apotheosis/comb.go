4dfed4d1ca88538bdf70f10664919aacce896f98
sundown <n@sundow.nl>
2022-02-21 17:08:04 +1300

    Fold and combine simplified

--- com/apotheosis/comb.go
+++ com/apotheosis/comb.go
@@ -11,9 +11,8 @@ import (
 // TODO define interface type for callable
 // https://hackage.haskell.org/package/base-4.16.0.0/docs/Prelude.html#v:zipWith
 func (env Environment) CombineOf(in interface{}, a, b Value) value.Value {
-	fn, ok := in.(prism.DyadicFunction)
 	var ret_typ prism.Type
-	if ok {
+	if fn, ok := in.(prism.DyadicFunction); ok {
 		ret_typ = fn.Type()
 	} else {
 		ret_typ = a.Type.(prism.VectorType).Type
@@ -28,20 +27,9 @@ func (env Environment) CombineOf(in interface{}, a, b Value) value.Value {
 	counter := env.Block.NewAlloca(types.I32)
 	env.Block.NewStore(I32(0), counter)
 
-	// New Vector of type [fn.Returns]
-	vectyp := prism.VectorType{Type: ret_typ}
-	newvec := env.Block.NewAlloca(vectyp.Realise())
-
 	len := env.ReadVectorLength(a)
 
-	env.WriteLLVectorLength(Value{newvec, vectyp}, len)
-	env.WriteLLVectorCapacity(Value{newvec, vectyp}, env.ReadVectorCapacity(a))
-
-	body := env.Block.NewBitCast(
-		env.Block.NewCall(env.GetCalloc(),
-			I32(ret_typ.Width()),
-			len),
-		types.NewPointer(ret_typ.Realise()))
+	newvec, body := env.LLVectorFactory(ret_typ, len)
 
 	env.Block.NewCondBr(
 		env.Block.NewICmp(enum.IPredEQ, len, env.ReadVectorLength(b)),
@@ -52,26 +40,15 @@ func (env Environment) CombineOf(in interface{}, a, b Value) value.Value {
 
 	lcount := loopblock.NewLoad(types.I32, counter)
 	call := env.Apply(in,
-		Value{
-			env.UnsafeReadVectorElement(a, lcount),
-			a.Type.(prism.VectorType).Type},
-		Value{
-			env.UnsafeReadVectorElement(b, lcount),
-			b.Type.(prism.VectorType).Type})
+		Value{env.UnsafeReadVectorElement(a, lcount), a.Type.(prism.VectorType).Type},
+		Value{env.UnsafeReadVectorElement(b, lcount), b.Type.(prism.VectorType).Type})
 
-	loopblock.NewStore(call,
-		loopblock.NewGetElementPtr(ret_typ.Realise(), body, lcount))
+	loopblock.NewStore(call, loopblock.NewGetElementPtr(ret_typ.Realise(), body, lcount))
 
 	loopblock.NewStore(loopblock.NewAdd(lcount, I32(1)), counter)
 
 	env.Block = env.CurrentFunction.NewBlock("")
-	loopblock.NewCondBr(
-		loopblock.NewICmp(enum.IPredNE, lcount, len),
-		loopblock,
-		env.Block)
+	loopblock.NewCondBr(loopblock.NewICmp(enum.IPredNE, lcount, len), loopblock, env.Block)
 
-	env.Block.NewStore(body,
-		env.Block.NewGetElementPtr(vectyp.Realise(), newvec, I32(0), vectorBodyOffset))
-
-	return newvec
+	return env.WriteVectorPointer(newvec, body, prism.VectorType{Type: ret_typ}.Realise())
 }
--- com/apotheosis/fold.go
+++ com/apotheosis/fold.go
@@ -10,48 +10,32 @@ import (
 
 func (env *Environment) CompileInlineFoldl(fn prism.Expression, vec Value) value.Value {
 	vectyp := vec.Type.(prism.VectorType).Type
-	lltyp := vectyp.Realise()
 
-	counter := env.Block.NewAlloca(types.I32)
 	len := env.ReadVectorLength(vec)
-	env.Block.NewStore(env.Block.NewSub(len, I32(3)), counter)
+	counter := env.New(env.Block.NewSub(len, I32(3)))
 
-	accum := env.Block.NewAlloca(fn.Type().Realise())
-
-	env.Block.NewStore(env.Apply(fn,
-		Value{
-			env.UnsafeReadVectorElement(vec, env.Block.NewSub(len, I32(2))),
-			vectyp},
-		Value{
-			env.UnsafeReadVectorElement(vec, env.Block.NewSub(len, I32(1))),
-			vectyp}), accum)
+	accum := env.New(env.Apply(fn,
+		Value{env.UnsafeReadVectorElement(vec, env.Block.NewSub(len, I32(2))), vectyp},
+		Value{env.UnsafeReadVectorElement(vec, env.Block.NewSub(len, I32(1))), vectyp}))
 
 	loopblock := env.CurrentFunction.NewBlock("")
 	exitblock := env.CurrentFunction.NewBlock("")
 
-	env.Block.NewCondBr(
-		env.Block.NewICmp(enum.IPredEQ, len, I32(2)),
-		exitblock,
-		loopblock)
+	env.Block.NewCondBr(env.Block.NewICmp(enum.IPredEQ, len, I32(2)), exitblock, loopblock)
 
 	env.Block = loopblock
 
 	lcount := loopblock.NewLoad(types.I32, counter)
-	loopblock.NewStore(env.Apply(fn,
-		Value{
-			env.UnsafeReadVectorElement(vec, lcount),
-			vectyp},
-		Value{
-			loopblock.NewLoad(lltyp, accum),
-			vectyp}), accum)
+	loopblock.NewStore(
+		env.Apply(fn,
+			Value{env.UnsafeReadVectorElement(vec, lcount), vectyp},
+			Value{loopblock.NewLoad(vectyp.Realise(), accum), vectyp}),
+		accum)
 
 	loopblock.NewStore(loopblock.NewSub(lcount, I32(1)), counter)
 
-	loopblock.NewCondBr(
-		loopblock.NewICmp(enum.IPredNE, lcount, I32(0)),
-		loopblock,
-		exitblock)
+	loopblock.NewCondBr(loopblock.NewICmp(enum.IPredNE, lcount, I32(0)), loopblock, exitblock)
 
 	env.Block = exitblock
-	return loopblock.NewLoad(lltyp, accum)
+	return env.Block.NewLoad(vectyp.Realise(), accum)
 }
