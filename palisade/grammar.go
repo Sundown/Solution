1a43adf6f4a8bb4e01b9112a379210c3f404c686
sundown <n@sundow.nl>
2022-03-01 05:40:24 +1300

    Parser tags

--- palisade/grammar.go
+++ palisade/grammar.go
@@ -2,38 +2,38 @@ package palisade
 
 type Expression struct {
 	// This order is extremely important
-	Operator  *Operator `( @@`
-	Monadic   *Monadic  `| @@`
-	Dyadic    *Dyadic   `| @@`
-	Morphemes *Morpheme `| @@ )`
+	Operator  *Operator `parser:"( @@"`
+	Monadic   *Monadic  `parser:"| @@"`
+	Dyadic    *Dyadic   `parser:"| @@"`
+	Morphemes *Morpheme `parser:"| @@ )"`
 }
 
 type Monadic struct {
-	Subexpr    *Expression `(("(" @@ ")")`
-	Verb       *Ident      `| @@)`
-	Expression *Expression `@@?`
+	Subexpr    *Expression `parser:"(('(' @@ ')')"`
+	Verb       *Ident      `parser:"| @@)"`
+	Expression *Expression `parser:"@@?"`
 }
 
 type Dyadic struct {
-	Monadic    *Monadic    `( @@`
-	Morphemes  *Morpheme   `| @@ )`
-	Subexpr    *Expression `(("(" @@ ")")`
-	Verb       *Ident      `| @@)`
-	Expression *Expression `@@?`
+	Monadic    *Monadic    `parser:"( @@"`
+	Morphemes  *Morpheme   `parser:"| @@ )"`
+	Subexpr    *Expression `parser:"(('(' @@ ')')"`
+	Verb       *Ident      `parser:"| @@)"`
+	Expression *Expression `parser:"@@?"`
 }
 
 type Operator struct {
-	Subexpr    *Expression `(("(" @@ ")")`
-	Verb       *Ident      `| @@)`
-	Operator   *string     `@Operator`
-	Expression *Expression `@@`
+	Subexpr    *Expression `parser:"(('(' @@ ')')"`
+	Verb       *Ident      `parser:"| @@)"`
+	Operator   *string     `parser:"@Operator"`
+	Expression *Expression `parser:"@@"`
 }
 
 type Morpheme struct {
-	Char   *[]string  `@Char+`
-	Alpha  *[]string  `| @Alpha+`
-	Omega  *[]string  `| @Omega+`
-	Real   *[]float64 `| @('-'? Float)+`
-	Int    *[]int64   `| @('-'? Int)+`
-	String *[]string  `| @String+`
+	Char   *[]string  `parser:"@Char+"`
+	Alpha  *[]string  `parser:"| @Alpha+"`
+	Omega  *[]string  `parser:"| @Omega+"`
+	Real   *[]float64 `parser:"| @('-'? Float)+"`
+	Int    *[]int64   `parser:"| @('-'? Int)+"`
+	String *[]string  `parser:"| @String+"`
 }
